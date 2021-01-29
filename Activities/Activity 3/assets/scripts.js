18b150e2c8bbdf321fd237f2cad1d79d65a69111
Rhenfel Apolonio <rlapolonio@gmail.com>
2021-01-29 15:07:33 +0800

    update activity 3

---
+++ Activities/Activity 3/assets/scripts.js
@@ -0,0 +1,21 @@
+/* DRAG AND DROP */
+
+document.querySelectorAll('.flex-tile').forEach(item => {
+    item.addEventListener('drop', event => {
+        event.preventDefault();
+        var data = event.dataTransfer.getData("text");
+
+        event.target.appendChild(document.getElementById(data));
+    });
+    item.addEventListener('dragover', event => {
+        event.preventDefault();
+    });
+});
+
+document.querySelectorAll('.piece').forEach(item => {
+    item.draggable = "true";
+    item.addEventListener('dragstart', event => {
+        event.dataTransfer.setData("text", event.target.id);
+    })
+});
+
