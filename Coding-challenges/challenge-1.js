20666efe366a30f51f5b9732eeacc2c33e03e329
Rhenfel Apolonio <rlapolonio@gmail.com>
2021-02-21 23:02:46 +0800

    revise challenge-1

--- Coding-challenges/challenge-1.js
+++ Coding-challenges/challenge-1.js
@@ -1,15 +1,24 @@
 var victorMass = 65; // store Victor's details in variables
-var victorHeight = 1.3;
+var victorHeight = 1.4;
 var victorBMI = victorMass / (victorHeight**2); // calc Victor's BMI
-
-var johnMass = 58; // store John's details in variables
+ 
+var johnMass = 57; // store John's details in variables
 var johnHeight = 1.4;
-var johnBMI = johnMass / (johnHeight**2); // calc John's BMI
+var johnBMI = johnMass / (johnHeight**2);  // calc John's BMI
 
 var comparison = victorBMI > johnBMI; // create a boolean variable on BMI comparison
 
+result = comparison ? 'YES' : 'NO'; // generate and store a string depending on the variable 'comparison'
+
+var victorDetails = `Victor's Mass: ${victorMass}kg, Victor's Height: ${victorHeight}m`;
+var johnDetails = `John's Mass: ${johnMass}kg, John's Height: ${johnHeight}m`;
+
+
 const text = "Is Victor's BMI higher than John's?";
 
-var message = `${text} ${comparison}`;
+var message = `${text} ${result}`;
 
-console.log(message); // display message + results
+
+console.log(victorDetails); // display message + initial values
+console.log(johnDetails); // display message + initial values
+console.log(message); // display message + result
